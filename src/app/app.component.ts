da97670cc5d07a8f5d67a916a3a2088c772f3611
Andrea Sanguin <andreasanguin@hotmail.it>
2023-12-19 23:14:05 +0100

    Build fixes, added comments to app.component.ts, updated app.module to handle routing and missing imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { Film, FilmsService } from './services/cinema.service';
-import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FormsModule], // Aggiunta di FormsModule qui
+  imports: [CommonModule, RouterOutlet, FormsModule],// Aggiungere RouterLink e RouterLinkActive se si vuole utilizzare "<a routerLink..."
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
+
+  //#region INIZIALIZZAZIONE VARIABILI/PROPRIETà DEL COMPONENTE
+
   films: Film[] = [];
   newFilm: Film = { id: 0, titolo: '', genere: '', durataMinuti: 0 };
   selectedFilm: Film | null = null;
   showFilmList: boolean = false;
   showAddFilmForm: boolean = false;
   showSelectedFilm: boolean = false;
-  
-  constructor(private filmsService: FilmsService, private router: Router) {}
+
+  title = 'cinema-client';
+
+  //#endregion
+
+  //#region COSTRUTTORE E ngOnInit SE PRESENTE
+
+  constructor(private filmsService: FilmsService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.loadFilms();
+  }
+
+  //#endregion
+
+  //#region LOGICA APPLICATIVA
 
   toggleFilmList() {
     this.showFilmList = !this.showFilmList;
@@ -31,15 +47,12 @@ export class AppComponent implements OnInit {
     this.router.navigateByUrl('/agg-film');
   }
 
-  ngOnInit(): void {
-    this.loadFilms();
-  }
-
   loadFilms(): void {
     this.filmsService.getFilmList().subscribe((films) => {
       this.films = films;
     });
   }
+
   addFilm(): void {
     if (this.newFilm.titolo && this.newFilm.genere && this.newFilm.durataMinuti > 0) {
       this.filmsService.addFilm(this.newFilm).subscribe((addedFilm) => {
@@ -49,18 +62,16 @@ export class AppComponent implements OnInit {
         this.showFilmList = true;
       });
     } else {
-      
       alert('Compila tutti i campi prima di aggiungere un film.');
     }
   }
-  
+
   updateFilm(film: Film): void {
     this.selectedFilm = film;
     this.showSelectedFilm = true;
     this.showFilmList = false;
     this.showAddFilmForm = false;
   }
-  
 
   saveUpdatedFilm(): void {
     if (this.selectedFilm) {
@@ -76,6 +87,7 @@ export class AppComponent implements OnInit {
     this.showFilmList = true;
     this.showSelectedFilm = false;
   }
+
   deleteFilm(id: number): void {
     this.filmsService.deleteFilm(id).subscribe(() => {
       this.films = this.films.filter((film) => film.id !== id);
@@ -86,5 +98,5 @@ export class AppComponent implements OnInit {
     this.newFilm = { id: 0, titolo: '', genere: '', durataMinuti: 0 };
   }
 
-  title = 'cinema-client';
+  //#endregion
 }
