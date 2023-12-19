fd800fabecde1e03b8ec359443489a2340b09cfb
Ludovico Paturzo <ludovico.paturzo@reti.it>
2023-12-19 14:57:57 +0100

    Primo caricamento
    È la versione no route, però va

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,97 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Film, FilmsService } from './services/cinema.service';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule], // Aggiunta di FormsModule qui
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  films: Film[] = [];
+  newFilm: Film = { id: 0, titolo: '', genere: '', durataMinuti: 0 };
+  selectedFilm: Film | null = null;
+  showFilmList: boolean = false;
+  showAddFilmForm: boolean = false;
+  showSelectedFilm: boolean = false;
+  
+  constructor(private filmsService: FilmsService) {}
+
+  toggleFilmList() {
+    this.showFilmList = !this.showFilmList;
+  }
+
+  toggleAddFilmForm() {
+    this.showAddFilmForm = !this.showAddFilmForm;
+    this.showFilmList = false;
+    this.showSelectedFilm = false;
+  }
+
+  ngOnInit(): void {
+    this.loadFilms();
+  }
+
+  loadFilms(): void {
+    this.filmsService.getFilmList().subscribe((films) => {
+      this.films = films;
+    });
+  }
+
+  addFilm(): void {
+    if (this.newFilm.titolo && this.newFilm.genere && this.newFilm.durataMinuti > 0) {
+      this.filmsService.addFilm(this.newFilm).subscribe((addedFilm) => {
+        this.films.push(addedFilm);
+        this.resetNewFilm();
+        this.showAddFilmForm = false;
+        this.showFilmList = true;
+      });
+    } else {
+      
+      alert('Compila tutti i campi prima di aggiungere un film.');
+    }
+  }
+  
+  updateFilm(film: Film): void {
+    this.selectedFilm = film;
+    this.showSelectedFilm = true;
+    this.showFilmList = false;
+    this.showAddFilmForm = false;
+  }
+  
+
+  saveUpdatedFilm(): void {
+    if (this.selectedFilm) {
+      this.filmsService.updateFilm(this.selectedFilm).subscribe((updatedFilm) => {
+        const index = this.films.findIndex((f) => f.id === updatedFilm.id);
+        if (index !== -1) {
+          this.films[index] = updatedFilm;
+          this.selectedFilm = null;
+          this.showAddFilmForm = false;
+        }
+      });
+    }
+    this.showFilmList = true;
+    this.showSelectedFilm = false;
+  }
+  
+  
+  
+
+  deleteFilm(id: number): void {
+    this.filmsService.deleteFilm(id).subscribe(() => {
+      this.films = this.films.filter((film) => film.id !== id);
+    });
+  }
+
+  resetNewFilm(): void {
+    this.newFilm = { id: 0, titolo: '', genere: '', durataMinuti: 0 };
+  }
+
   title = 'cinema-client';
 }
---
+++ src/app/services/cinema.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable()
+export class FilmsService {
+  constructor(private http: HttpClient) { }
+
+  // getFilmList(){
+  //   this.http.get<Film[]>("https://localhost:7160/api/Gestore")
+  //   .subscribe((films) => {
+  //       console.log(films.map(f => f.titolo));
+  //   })
+  // }
+  getFilmList(): Observable<Film[]> {
+    return this.http.get<Film[]>("https://localhost:7160/api/Gestore");
+  }
+
+  addFilm(film: Film): Observable<Film> {
+    return this.http.post<Film>("https://localhost:7160/api/Gestore", film);
+  }
+
+  updateFilm(film: Film): Observable<Film> {
+    return this.http.put<Film>(`https://localhost:7160/api/Gestore/${film.id}`, film);
+  }
+
+  deleteFilm(id: number): Observable<void> {
+    return this.http.delete<void>(`https://localhost:7160/api/Gestore/${id}`);
+  }
+
+ 
+}
+
+export class Film
+{    
+    public id: number;
+    public genere: string;
+    public durataMinuti: number;
+    public titolo: string;
+}
